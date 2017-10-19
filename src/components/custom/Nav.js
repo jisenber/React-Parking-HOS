16e1947021ce791747f39bb1e078318ad34fc505
jisenber <isenberg.jacob@yahoo.com>
2017-10-19 14:45:55 -0700

    made registration and and login options available when clicking on mobile hamburger. Also added some styling.

--- src/components/custom/Nav.js
+++ src/components/custom/Nav.js
@@ -4,6 +4,7 @@ import { Button, Navbar, NavbarBrand, NavbarNav, NavbarToggler, NavItem, NavLink
 import Routes from '../../../mdbReactdocs/Routes';
 import Register from './Register';
 import Login from './Login';
+import '../../style/style.css';
 
 //Nav Component
 class Nav extends Component {
@@ -13,34 +14,51 @@ class Nav extends Component {
     this.state = {
       login: false,
       register: false,
-      backdrop: false
+      backdrop: false,
+      mobileNavOptions: false
     };
 
+    //binds the function to the component. Without these bindings 'this' would not have the proper reference
     this.toggleRegister = this.toggleRegister.bind(this);
     this.toggleLogin = this.toggleLogin.bind(this);
+    this.toggleMobileNav = this.toggleMobileNav.bind(this);
 
   }
 
+  //opens registration modal
   toggleRegister() {
     this.setState({
       register: !this.state.register
     });
   }
 
+  //opens login modal
   toggleLogin() {
     this.setState({
       login: !this.state.login
     });
   }
 
+  toggleMobileNav(e) {
+    e.preventDefault()
+    console.log('toggled');
+    this.setState({
+      mobileNavOptions: !this.state.mobileNavOptions
+    });
+  }
+
+
   render() {
     return (
       <div className="flyout">
         <Navbar color="black" dark expand="md">
           <NavbarBrand href="/">Space Invaderz</NavbarBrand>
-          <NavbarToggler/>
+          <NavbarToggler onClick={this.toggleMobileNav}/>
+          <ul className="dropdown-menu"  style={{display: this.state.mobileNavOptions ? 'block' : 'none'}}>
+            <li className ="navList" onClick={this.toggleLogin}><b>Login</b></li>
+            <li className ="navList" onClick={this.toggleRegister}><b>Register</b></li>
+          </ul>
           <div className="collapse navbar-collapse" id="reactNavbar">
-
             <NavbarNav className="ml-auto">
               <NavItem>
                 <Button onClick={this.toggleLogin}>Login</Button>
@@ -52,16 +70,6 @@ class Nav extends Component {
           </div>
         </Navbar>
         <Routes />
-        <Modal isOpen={this.state.login} toggle={this.toggleLogin} backdrop="static">
-          <ModalHeader toggle={this.toggle}>Login Modal</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary">Login</Button>{' '}
-            <Button color="secondary" onClick={this.toggleLogin}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
         <Register isOpen = {this.state.register} toggle={this.toggleRegister} backdrop ="static"/>
         <Login isOpen = {this.state.login} toggle={this.toggleLogin} backdrop ="static"/>
       </div>
